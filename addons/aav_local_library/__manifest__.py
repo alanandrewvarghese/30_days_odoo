8b63561442a3b7270b4153340995e72dee474b73
alanandrewvarghese <alanandrew883@gmail.com>
2025-12-14 20:48:38 +0530

    @api.depends | Computed Fields

--- addons/aav_local_library/__manifest__.py
+++ addons/aav_local_library/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Local Library Management",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.4",
     "summary": "Manage your library effortlessly.",
     "description": """
         This module helps librarians manage the book collection.
--- addons/aav_local_library/models/res_partner.py
+++ addons/aav_local_library/models/res_partner.py
@@ -4,4 +4,22 @@ class ResPartner(models.Model):
     _inherit="res.partner"
     
     authored_book_ids = fields.One2many("library.book", "author_id", string="Authored Books")
-    is_author = fields.Boolean(string="Is Author")
+    is_author = fields.Boolean(string="Is Author", compute="_compute_is_author", store=True, readonly=True)
+    total_pages = fields.Integer(string="Total Pages", compute="_compute_total_pages", store=True, readonly=True)
+    
+    @api.depends("authored_book_ids")
+    def _compute_is_author(self):
+        for partner in self:
+            if partner.authored_book_ids:
+                partner.is_author = True
+            else:
+                partner.is_author = False
+                
+    @api.depends("authored_book_ids", "authored_book_ids.pages")
+    def _compute_total_pages(self):
+        for partner in self:
+            if partner.authored_book_ids:
+                partner.total_pages = sum(partner.authored_book_ids.mapped('pages'))
+            else:
+                partner.total_pages = 0
+                
